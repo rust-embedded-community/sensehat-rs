4df27b21575cc509b3cc02112709534e8722807d
Jonathan Pallant <github@thejpster.org.uk>
2017-02-18 23:30:07 +0000

    Clean up example.

--- examples/get_all.rs
+++ examples/get_all.rs
@@ -5,11 +5,9 @@ use sensehat_rs::SenseHat;
 fn main() {
     let mut sense_hat = SenseHat::new().expect("Couldn't create Sense Hat object");
     let temp = sense_hat.get_temperature_from_humidity().expect("Couldn't get temp");
-    println!("It's {:.2}\u{00B0} Celcius on the humidity sensor",
-             temp.as_celsius());
+    println!("It's {} on the humidity sensor", temp);
     let temp = sense_hat.get_temperature_from_pressure().expect("Couldn't get temp");
-    println!("It's {:.2}\u{00B0} Celcius on the pressure sensor",
-             temp.as_celsius());
+    println!("It's {} on the pressure sensor", temp);
     let rh = sense_hat.get_humidity().expect("Couldn't get rh");
     println!("It's {} relative humidity", rh);
     let pressure = sense_hat.get_pressure().expect("Couldn't get pressure");
