359f513bec99b29fd093ffa86c9b48bf4fca93a7
Jonathan Pallant <github@thejpster.org.uk>
2017-06-18 20:29:49 +0100

    Ran cargo fmt.

--- examples/get_all.rs
+++ examples/get_all.rs
@@ -4,9 +4,13 @@ use sensehat::SenseHat;
 
 fn main() {
     let mut sense_hat = SenseHat::new().expect("Couldn't create Sense Hat object");
-    let temp = sense_hat.get_temperature_from_humidity().expect("Couldn't get temp");
+    let temp = sense_hat
+        .get_temperature_from_humidity()
+        .expect("Couldn't get temp");
     println!("It's {} on the humidity sensor", temp);
-    let temp = sense_hat.get_temperature_from_pressure().expect("Couldn't get temp");
+    let temp = sense_hat
+        .get_temperature_from_pressure()
+        .expect("Couldn't get temp");
     println!("It's {} on the pressure sensor", temp);
     let rh = sense_hat.get_humidity().expect("Couldn't get rh");
     println!("It's {} relative humidity", rh);
