d12573716a36660af5b6d0ec9aa320622a292aa0
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2018-04-08 18:30:58 +0100

    Designing new API for screen.

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,6 @@ extern crate measurements;
 #[cfg(feature = "rtimu")]
 extern crate libc;
 
-#[cfg(feature = "led-matrix")]
 extern crate sensehat_screen;
 
 mod rh;
@@ -34,6 +33,8 @@ pub use measurements::Pressure;
 pub use measurements::Angle;
 pub use rh::RelativeHumidity;
 
+pub use sensehat_screen::{FrameLine, PixelColor, Screen};
+
 use i2cdev::linux::{LinuxI2CDevice, LinuxI2CError};
 
 #[cfg(feature = "rtimu")]
@@ -43,6 +44,30 @@ mod lsm9ds1;
 mod lsm9ds1_dummy;
 #[cfg(not(feature = "rtimu"))]
 use lsm9ds1_dummy as lsm9ds1;
+
+pub const LED_HEIGHT: u8 = 8;
+pub const LED_WIDTH: u8 = 8;
+pub const LED_NUM_PIXELS: usize = LED_HEIGHT as usize * LED_WIDTH as usize;
+
+/// Represents a specific pixel position on the LED
+#[derive(Debug, Copy, Clone)]
+pub struct PixelPosition {
+    x: u8,
+    y: u8
+}
+
+/// How to rotate the image on the LED display
+#[derive(Debug, Copy, Clone)]
+pub enum Rotation {
+    /// Don't rotate image - top is near GPIO pins
+    Normal,
+    /// Rotate image 90 degrees clockwise - top is near USB ports
+    Clockwise90,
+    /// Rotate image 180 degrees clockwise - top is near HDMI port
+    Clockwise180,
+    /// Rotate image 270 degrees clockwise - top is near micro SD card
+    Clockwise270
+}
 
 /// Represents an orientation from the IMU
 #[derive(Debug, Copy, Clone)]
@@ -62,6 +87,12 @@ pub struct SenseHat<'a> {
     accelerometer_chip: lsm9ds1::Lsm9ds1<'a>,
     /// Cached data
     orientation: Orientation,
+    /// LED matrix rotation
+    rotation: Rotation,
+    /// Current LED contents
+    image: Image,
+    /// Handle to the framebuffer
+    screen: Screen
 }
 
 /// Errors that this crate can return
@@ -69,12 +100,27 @@ pub struct SenseHat<'a> {
 pub enum SenseHatError {
     NotReady,
     GenericError,
+    PositionOutOfBounds,
     I2CError(LinuxI2CError),
     LSM9DS1Error(lsm9ds1::Error),
+    FramebufferError(sensehat_screen::FramebufferError)
 }
+
+/// An image on the LED matrix
+#[derive(Clone)]
+pub struct Image([PixelColor; LED_NUM_PIXELS]);
 
 /// A shortcut for Results that can return `T` or `SenseHatError`
 pub type SenseHatResult<T> = Result<T, SenseHatError>;
+
+/// Draw mode
+#[derive(Debug, Copy, Clone)]
+pub enum DrawMode {
+    /// Write this change to the display now
+    OutputNow,
+    /// Buffer this change internally
+    BufferInternally
+}
 
 impl<'a> SenseHat<'a> {
     /// Try and create a new SenseHat object.
@@ -82,6 +128,7 @@ impl<'a> SenseHat<'a> {
     /// Will open the relevant I2C devices and then attempt to initialise the
     /// chips on the Sense HAT.
     pub fn new() -> SenseHatResult<SenseHat<'a>> {
+        let blue_pixel = PixelColor::new(0, 0, 255);
         Ok(SenseHat {
             humidity_chip: hts221::Hts221::new(LinuxI2CDevice::new("/dev/i2c-1", 0x5f)?)?,
             pressure_chip: lps25h::Lps25h::new(LinuxI2CDevice::new("/dev/i2c-1", 0x5c)?)?,
@@ -91,6 +138,9 @@ impl<'a> SenseHat<'a> {
                 pitch: Angle::from_degrees(0.0),
                 yaw: Angle::from_degrees(0.0),
             },
+            rotation: Rotation::Normal,
+            image: Image([blue_pixel; LED_NUM_PIXELS]),
+            screen: Screen::open("/dev/fb1")?
         })
     }
 
@@ -186,6 +236,119 @@ impl<'a> SenseHat<'a> {
             Err(SenseHatError::NotReady)
         }
     }
+
+    /// Set the LED matrix rotation
+    pub fn set_rotation(&mut self, rotation: Rotation, redraw: DrawMode) {
+        self.rotation = rotation;
+        self.image.rotate_mut(rotation);
+        match redraw {
+            DrawMode::OutputNow => self.redraw(),
+            _ => {}
+        }
+    }
+
+    /// Get the current LED matrix rotation
+    pub fn get_rotation(&self) -> Rotation {
+        self.rotation
+    }
+
+    /// Set the whole bufferd image.
+    pub fn set_pixels(&mut self, image: Image) -> SenseHatResult<()> {
+        self.image = image;
+        self.redraw();
+        Ok(())
+    }
+
+    /// Get the whole buffered image.
+    pub fn get_pixels(&self) -> Image {
+        return self.image.clone()
+    }
+
+    /// Set the colour of a single pixel.
+    pub fn set_pixel(&mut self, position: PixelPosition, color: PixelColor, redraw: DrawMode) -> SenseHatResult<()> {
+        if position.valid() {
+            self.image.0[position.pixel()] = color;
+            match redraw {
+                DrawMode::OutputNow => self.redraw(),
+                _ => {}
+            }
+            Ok(())
+        } else {
+            Err(SenseHatError::PositionOutOfBounds)
+        }
+    }
+
+    /// Get the colour of a single pixel.
+    pub fn get_pixel(&mut self, position: PixelPosition) -> SenseHatResult<PixelColor> {
+        if position.valid() {
+            Ok(self.image.0[position.pixel()])
+        } else {
+            Err(SenseHatError::PositionOutOfBounds)
+        }
+    }
+
+    /// Scroll a message across the screen. Blocks until completion.
+    pub fn show_message(&mut self, message: &str, speed: f32, text: PixelColor, background: PixelColor) -> SenseHatResult<()> {
+        println!("Would should show {:?} at {} seconds/frame in {:?}/{:?}", message, speed, text, background);
+        Ok(())
+    }
+
+    /// Write a single character to the screen
+    pub fn show_letter(&mut self, letter: char, text: PixelColor, background: PixelColor) -> SenseHatResult<()> {
+        println!("Would should show {:?} in {:?}/{:?}", letter, text, background);
+        Ok(())
+    }
+
+    /// Clear the display
+    pub fn clear(&mut self, color: PixelColor, redraw: DrawMode) -> SenseHatResult<()> {
+        for pixel in self.image.0.iter_mut() {
+            *pixel = color;
+        }
+        match redraw {
+            DrawMode::OutputNow => self.redraw(),
+            _ => {}
+        }
+        Ok(())
+    }
+
+    pub fn redraw(&mut self) {
+        let image = self.image.rotate_copy(self.rotation);
+        let frame = FrameLine::from_pixels(&image.0);
+        self.screen.write_frame(&frame);
+    }
+}
+
+impl PixelPosition {
+    fn valid(&self) -> bool {
+        (self.x < LED_WIDTH) && (self.y < LED_HEIGHT)
+    }
+
+    fn pixel(&self) -> usize {
+        usize::from(self.x) + (usize::from(self.y) * usize::from(LED_HEIGHT))
+    }
+}
+
+impl Image {
+    fn rotate_mut(&mut self, rotation: Rotation) {
+        match rotation {
+            Rotation::Normal => {},
+            Rotation::Clockwise90 => {
+                unimplemented!()
+            }
+            Rotation::Clockwise180 => {
+                unimplemented!()
+            }
+            Rotation::Clockwise270 => {
+                unimplemented!()
+            }
+        }
+    }
+
+    fn rotate_copy(&self, rotation: Rotation) -> Image {
+        let mut im = self.clone();
+        im.rotate_mut(rotation);
+        im
+    }
 }
 
 impl From<LinuxI2CError> for SenseHatError {
@@ -197,6 +360,12 @@ impl From<LinuxI2CError> for SenseHatError {
 impl From<lsm9ds1::Error> for SenseHatError {
     fn from(err: lsm9ds1::Error) -> SenseHatError {
         SenseHatError::LSM9DS1Error(err)
+    }
+}
+
+impl From<sensehat_screen::FramebufferError> for SenseHatError {
+    fn from(err: sensehat_screen::FramebufferError) -> SenseHatError {
+        SenseHatError::FramebufferError(err)
     }
 }
 
