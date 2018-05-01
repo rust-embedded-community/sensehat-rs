6139da5ce37c2970a2930bb97da86980d2216902
Joaquin Rosales <globojorro@gmail.com>
2018-05-01 00:03:32 -0500

    update examples/compass_screen.rs to use latest screen version

--- examples/compass_screen.rs
+++ examples/compass_screen.rs
@@ -1,8 +1,8 @@
 extern crate sensehat;
 extern crate sensehat_screen;
 
-use sensehat::{SenseHat, FrameLine, PixelColor, Screen};
-use sensehat_screen::PixelFrame;
+use sensehat::{SenseHat, PixelColor, Screen};
+use sensehat_screen::{PixelFrame, Rotate};
 
 const DARK: PixelColor = PixelColor::BLACK;
 const BLUE: PixelColor = PixelColor::BLUE;
@@ -12,7 +12,7 @@ fn main() {
     let mut sense_hat = SenseHat::new().expect("Couldn't create Sense HAT object");
     let mut screen = Screen::open("/dev/fb1").expect("Couldn't find Sense HAT LED matrix");
 
-    let background_pixels = vec![ 
+    let background_pixels = [
         BLUE, BLUE, DARK, DARK, DARK, DARK, BLUE, BLUE, // 0-7
         BLUE, DARK, BLUE, BLUE, BLUE, BLUE, DARK, BLUE, // 8-15
         DARK, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, DARK, // 16-23
@@ -26,15 +26,15 @@ fn main() {
     let background = PixelFrame::new(&background_pixels);
 
     let right = {
-        let mut pxs = background_pixels.clone();
+        let mut pxs = background_pixels;
         pxs[23] = RED.dim(0.5);
         pxs[47] = RED;
         pxs[31] = RED;
-        pxs[39] = RED;.dim(0.5)
+        pxs[39] = RED.dim(0.5);
         PixelFrame::new(&pxs)
     };
     let right_up = {
-        let mut pxs = background_pixels.clone();
+        let mut pxs = background_pixels;
         pxs[5] = RED.dim(0.5);
         pxs[14] = RED;
         pxs[23] = RED;
@@ -42,7 +42,7 @@ fn main() {
         PixelFrame::new(&pxs)
     };
     let right_down = {
-        let mut pxs = background_pixels.clone();
+        let mut pxs = background_pixels;
         pxs[39] = RED.dim(0.5);
         pxs[47] = RED;
         pxs[54] = RED;
@@ -50,17 +50,17 @@ fn main() {
         PixelFrame::new(&pxs)
     };
 
-    let up = right.rotate_left();
-    let up_left = right_up.rotate_left();
-    let up_right = right_down.rotate_left();
+    let up = right.rotate(Rotate::Ccw90);
+    let up_left = right_up.rotate(Rotate::Ccw90);
+    let up_right = right_down.rotate(Rotate::Ccw90);
 
-    let left = right.rotate_180();
-    let left_up = right_down.rotate_180();
-    let left_down = right_up.rotate_180();
+    let left = right.rotate(Rotate::Ccw180);
+    let left_up = right_down.rotate(Rotate::Ccw180);
+    let left_down = right_up.rotate(Rotate::Ccw180);
 
-    let down = up.rotate_180();
-    let down_left = up_right.rotate_180();
-    let down_right = up_left.rotate_180();
+    let down = up.rotate(Rotate::Ccw180);
+    let down_left = up_right.rotate(Rotate::Ccw180);
+    let down_right = up_left.rotate(Rotate::Ccw180);
 
     screen.write_frame(&background.frame_line());
 
