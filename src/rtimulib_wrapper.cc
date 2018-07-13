71d54e6a989995f5fdeaee24e23c093cf33b1806
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2018-07-13 14:17:05 +0100

    Remove some magic, and rtimulib wrapper frees in the right order.

--- src/rtimulib_wrapper.cc
+++ src/rtimulib_wrapper.cc
@@ -5,7 +5,6 @@
 struct WrapperContext {
     RTIMUSettings* p_settings;
     RTIMU* p_imu;
-    uint32_t magic;
 };
 
 struct Vector3D {
@@ -37,13 +36,11 @@ extern "C" {
     void rtimulib_wrapper_destroy(WrapperContext* p_context);
     void rtimulib_set_sensors(WrapperContext* p_context, int gyro, int accel, int compass);
     int rtimulib_wrapper_imu_read(WrapperContext* p_context);
-    int rtimulib_wrapper_get_imu_fusion_pose(WrapperContext* p_context, Vector3D* p_output);
+    int rtimulib_wrapper_get_imu_data(WrapperContext* p_context, AllData* p_output);
 }
 
 WrapperContext* rtimulib_wrapper_create(void) {
     WrapperContext* p_context = new WrapperContext;
-    p_context->magic = 0xDEADBEEF;
-    printf("Magic at create is %08x\n", p_context->magic);
     // TODO: Should be ~/.config/sense_hat/RTIMULib
     p_context->p_settings = new RTIMUSettings("RTIMULib");
     p_context->p_imu = RTIMU::createIMU(p_context->p_settings);
@@ -54,9 +51,9 @@ WrapperContext* rtimulib_wrapper_create(void) {
 }
 
 void rtimulib_wrapper_destroy(WrapperContext* p_context) {
-    printf("Magic at destroy is %08x\n", p_context->magic);
-    delete p_context->p_settings;
+    // The settings object must outlive the IMU object
     delete p_context->p_imu;
+    delete p_context->p_settings;
     delete p_context;
 }
 
