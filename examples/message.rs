0f9517c8e28b66dcdc8d3e923142fea93daa237a
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2018-07-21 21:46:43 +0100

    Scroll faster.

--- examples/message.rs
+++ examples/message.rs
@@ -5,8 +5,8 @@ use sensehat::{Fps, SenseHat};
 fn main() {
     let mut sense_hat = SenseHat::new().unwrap();
     let temp = sense_hat.get_temperature_from_humidity().unwrap();
-    let msg = format!("It's {}", temp);
+    let msg = format!("It's {:.1}°C  ", temp.as_celsius());
     loop {
-	    sense_hat.show_message(&msg, Fps(1), "green", "black").unwrap();
+	    sense_hat.show_message(&msg, Fps(8), "green", "black").unwrap();
     }
 }
